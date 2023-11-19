bbd4ecc21c27d0d9bb88443724a933a5114cc748
shorif <shorif.tk@gmail.com>
2023-11-19 23:47:24 +0600

    first commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 
 class ProfileController extends Controller
 {
-    public function index($id)
+    public function index(Request $request)
     {
 
         $name = "Donal Trump";
         $age = "75";
+        $id = $request->id;
 
         $data = [
             "id" => $id,
